d2e67fe7211ce25cceefe010fd1d9950de128ab0
stein.pedersen <stein.pedersen@bluware.com>
2025-01-21 08:21:18 +0100

    Disable DeprecationWarning.

--- hexview.bat
+++ hexview.bat
@@ -1,3 +1,3 @@
 @SETLOCAL
-@FOR /F "usebackq delims=" %%i IN (`python -c "import sys; print(f'{sys.exec_prefix}\\Scripts',end='')"`) DO SET SCRIPTS_PATH=%%i
-python %SCRIPTS_PATH%\hexview %*
+@FOR /F "usebackq delims=" %%i IN (`python -c "import sys; print(f'{sys.exec_prefix}\\Scripts',end='')"`) DO @SET SCRIPTS_PATH=%%i
+python -W ignore::DeprecationWarning %SCRIPTS_PATH%\hexview %*
